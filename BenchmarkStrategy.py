17b820592938049606ed2f728716845aaea90286
Max Zhalilo <maxzhalilo@example.com>
2025-09-30 21:19:21 -0500

    Added strategycomp jupyter nb

--- BenchmarkStrategy.py
+++ BenchmarkStrategy.py
@@ -4,11 +4,11 @@ class BenchmarkStrategy(Strategy):
     """
     Implements a benchmark strategy.
     """
-    def generate_signals(self):
+    def generate_signals(self, x):
         """
         Generates trading signals based on the benchmark logic.
         """
         signals_df = self.data.copy()
         signals_df['signal'] = 0
-        signals_df.iloc[0]['signal'] = 1
+        signals_df.iloc[0]['signal'] = x
         return signals_df
