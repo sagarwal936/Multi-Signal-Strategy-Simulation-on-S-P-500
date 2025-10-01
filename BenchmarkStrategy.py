a8e199b43809bfd3dfdd9f3791f90d88161979a6
Max Zhalilo <maxzhalilo@example.com>
2025-09-30 20:31:47 -0500

    Add BenchmarkStrategy implementation

---
+++ BenchmarkStrategy.py
@@ -0,0 +1,14 @@
+from Strategy import Strategy
+
+class BenchmarkStrategy(Strategy):
+    """
+    Implements a benchmark strategy.
+    """
+    def generate_signals(self):
+        """
+        Generates trading signals based on the benchmark logic.
+        """
+        signals_df = self.data.copy()
+        signals_df['signal'] = 0
+        signals_df.iloc[0]['signal'] = 1
+        return signals_df
